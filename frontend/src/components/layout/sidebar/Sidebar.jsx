bb5a6d666fc30acdfb5b65b55979ad08beeb8fce
Emerson Henrique <emerson.comar@estudante.uffs.edu.br>
2025-05-28 07:53:51 -0300

    Ajustado consistencia do sidebar. Capturado clique do submenu

--- frontend/src/components/layout/sidebar/Sidebar.jsx
+++ frontend/src/components/layout/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import SidebarFooter from './SidebarFooter';
 
 // Adicionar prop 'group'
 const Sidebar = ({ user, currentPage }) => {
-  const [ actPage, setActPage ] = useState(currentPage);
+  const [actPage, setActPage] = useState(currentPage);
 
   const optNavegacao = [
     {
@@ -15,27 +15,30 @@ const Sidebar = ({ user, currentPage }) => {
     {
       name: 'Rebanho',
       icon: './sheep_icon.svg',
-      submenu: [{ name: 'Cadastro' }, { name: 'Listar' }, { name: 'Pesagem' }]
+      submenu: [{ name: 'Cadastro' }, { name: 'Listar' }, { name: 'Pesagem' }],
     },
     {
       name: 'Calendário',
-      icon: './calendar.svg'
+      icon: './calendar.svg',
     },
     {
-      name: 'Relatórios',
+      name: 'Relatório',
       icon: './report.svg',
-      submenu: [{ name: 'Cadastrados' }, { name: 'SIEM' }]
     },
     {
       name: 'Usuários',
       icon: './people_icon.svg',
-      submenu: [{ name: 'Cadastrar' }, { name: 'Listar' }, { name: 'Grupos' }]
-    }
+      submenu: [{ name: 'Cadastrar' }, { name: 'Listar' }, { name: 'Grupos' }],
+    },
   ];
 
   const paginaSelecionada = (name) => {
     console.log(`DEBUG: Clicado em ${name}`);
-    setActPage((prev) => (prev === name? null: name));
+    setActPage((prev) => (prev = name));
+  };
+
+  const submenuSelecionado = (name) => {
+    console.log(`DEBUG: Clicado no submenu ${name}`);
   };
 
   return (
@@ -43,10 +46,14 @@ const Sidebar = ({ user, currentPage }) => {
       <div>
         <SidebarHeader user={user} profilePicture={'./Group_2.png'} />
         <div className="row pt-3 m-0">
-          {optNavegacao.map(option => (
+          {optNavegacao.map((option) => (
             <div className="row w-100 mx-0 px-0" key={option.name}>
-              <div  className={`row justify-content-center w-100 m-0 p-0 ${option.name === actPage ? 'active' : null}`}>
-                <div className='opt-cont'>
+              <div
+                className={`row justify-content-center w-100 m-0 p-0 ${
+                  option.name === actPage ? 'active' : ''
+                }`}
+              >
+                <div className="opt-cont">
                   <NavOption
                     name={option.name}
                     icon={option.icon}
@@ -58,7 +65,14 @@ const Sidebar = ({ user, currentPage }) => {
               {actPage === option.name && option.submenu && (
                 <div className="submenu text-center">
                   {option.submenu.map((sub) => (
-                    <p key={sub.name} className="submenu-item">{sub.name}</p>
+                    <p
+                      key={sub.name}
+                      className="submenu-item"
+                      onClick={() => submenuSelecionado(sub.name)}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      {sub.name}
+                    </p>
                   ))}
                 </div>
               )}
@@ -70,6 +84,5 @@ const Sidebar = ({ user, currentPage }) => {
     </nav>
   );
 };
-
 
 export default Sidebar;
